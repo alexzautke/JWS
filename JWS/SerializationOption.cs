296399ed73d1486d59c563d6449649f6577a59f1
Alexander Zautke <mail@alexander-zautke.com>
2018-12-24 12:05:30 +0100

    Add SerializationOption

---
+++ JWS/SerializationOption.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CreativeCode.JWS
+{
+    public sealed class SerializationOption
+    {
+        public static readonly SerializationOption JWSCompactSerialization = new SerializationOption("JWS Compact Serialization");
+        public static readonly SerializationOption JWSJSONSerialization = new SerializationOption("JWS JSON Serialization");
+
+        private readonly string value;
+
+        private SerializationOption(string value)
+        {
+            this.value = value;
+        }
+
+        public override string ToString()
+        {
+            return value;
+        }
+
+    }
+}

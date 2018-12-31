659bb9606eab0f08fdac26970c8bfe39a5e3b6b7
Alexander Zautke <mail@alexander-zautke.com>
2018-12-31 15:47:07 +0100

    Base64urlDecode

--- JWS/JWS.cs
+++ JWS/JWS.cs
@@ -107,6 +107,25 @@ namespace CreativeCode.JWS
             return base64;
         }
 
+        private byte[] Base64urlDecode(string arg)
+        {
+            string s = arg;
+            s = s.Replace('-', '+'); // 62nd char of encoding
+            s = s.Replace('_', '/'); // 63rd char of encoding
+            switch (s.Length % 4) // Pad with trailing '='s
+            {
+                case 0: 
+                    break; // No pad chars in this case
+                case 2: 
+                    s += "=="; break; // Two pad chars
+                case 3: 
+                    s += "="; break; // One pad char
+                default: 
+                    throw new System.Exception("Illegal base64url string!");
+            }
+            return Convert.FromBase64String(s); // Standard base64 decoder
+        }
+
         private void CheckNotNull(object toCheck, string argumentName)
         {
             if (toCheck == null)

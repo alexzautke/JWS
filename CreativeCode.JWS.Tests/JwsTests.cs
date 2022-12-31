8e3130e8a5c1d19ee1e52276c8b8ed1a3a88da1f
Alexander Zautke <alexander@fire.ly>
2022-12-31 14:12:47 +0100

    Update .sln and .csproject

---
+++ CreativeCode.JWS.Tests/JwsTests.cs
@@ -0,0 +1,23 @@
+using System.Text;
+using CreativeCode.JWK.KeyParts;
+using Xunit;
+
+namespace CreativeCode.JWS.Tests;
+
+public class JwsTests
+{
+    
+    [Fact]
+    public void CompactJwsWithRsaSignatureCanBeSerialized()
+    {
+        var keyUse = PublicKeyUse.Signature;
+        var keyOperations = new HashSet<KeyOperation>(new[] {KeyOperation.ComputeDigitalSignature, KeyOperation.VerifyDigitalSignature});
+        var algorithm = Algorithm.RS256;
+        var jwk = new JWK.JWK(algorithm, keyUse, keyOperations);
+        var joseHeader = new ProtectedJoseHeader(jwk, "application/fhir+json", SerializationOption.JWSCompactSerialization);
+        var payload = Encoding.UTF8.GetBytes("payload");
+
+        var jws = new JWS(joseHeader, payload);
+        var json = jws.Export();
+    }
+}

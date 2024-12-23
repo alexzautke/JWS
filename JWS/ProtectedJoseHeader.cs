34f6548a77f15f04a8f7ca9af960f2958ba8b3ad
Alexander Zautke <alexander@fire.ly>
2024-12-23 13:34:27 +0300

    Add support for ‘crit’ header

--- JWS/ProtectedJoseHeader.cs
+++ JWS/ProtectedJoseHeader.cs
@@ -30,6 +30,9 @@ namespace CreativeCode.JWS
 
     public class ProtectedJoseHeader
     {
+        private static IReadOnlyList<string> _jsonPropertyNamesDefinedByJws = new[]
+            {"alg", "jwk", "kid", "typ", "cty", "crit", "x5t#S256", "x5t", "x5c", "x5u", "jku"};
+            
         [JsonProperty(PropertyName = "alg")]
         [JWSConverterAttribute(typeof(AlgorithmConverter))]
         public Algorithm Algorithm { get; internal set; }      // REQUIRED, must match the 'alg' value of the supplied JWK
@@ -50,7 +53,11 @@ namespace CreativeCode.JWS
         
         [JsonProperty()]
         [JWSConverterAttribute(typeof(AdditionalHeadersConverter))]
-        public IReadOnlyDictionary<string, string> AdditionalHeaders { get; internal set; } // OPTIONAL
+        public IReadOnlyDictionary<string, object> AdditionalHeaders { get; internal set; } // OPTIONAL
+        
+        [JsonProperty("crit")]
+        [JWSConverterAttribute(typeof(CriticalHeadersConverter))]
+        public IReadOnlyList<string> CriticalHeaders { get; internal set; } // OPTIONAL
 
         public ProtectedJoseHeader(JWK.JWK jwk, string contentType, SerializationOption serializationOption)
         {
@@ -66,8 +73,10 @@ namespace CreativeCode.JWS
             Type = serializationOption;
             ContentType = ShortenContentType(contentType);
         }
-        
-        public ProtectedJoseHeader(JWK.JWK jwk, SerializationOption serializationOption, string contentType = null, IReadOnlyDictionary<string, string> additionalHeaders = null)
+
+        public ProtectedJoseHeader(JWK.JWK jwk, SerializationOption serializationOption, string contentType = null,
+            IReadOnlyDictionary<string, object> additionalHeaders = null,
+            IReadOnlyList<string> criticalHeaders = null)
         {
             if (jwk is null)
                 throw new ArgumentNullException("jwk MUST be provided");
@@ -78,6 +87,34 @@ namespace CreativeCode.JWS
             Type = serializationOption;
             ContentType = ShortenContentType(contentType);
             AdditionalHeaders = additionalHeaders;
+
+            CheckCriticalHeaders(criticalHeaders, additionalHeaders);
+            CriticalHeaders = criticalHeaders;
+        }
+        
+        /*
+           Producers
+           MUST NOT include Header Parameter names defined by this specification
+           or [JWA] for use with JWS, duplicate names, or names that do not
+           occur as Header Parameter names within the JOSE Header in the "crit"
+           list.
+           
+            See RFC7515 - Section 4.1.11.  "crit" (Critical) Header Parameter
+         */
+
+        private void CheckCriticalHeaders(IReadOnlyList<string> criticalHeaders, IReadOnlyDictionary<string, object> additionalHeaders)
+        {
+            if (criticalHeaders is null)
+                return;
+            
+            if (criticalHeaders.Count != criticalHeaders.Distinct().Count())
+                throw new ArgumentException("Values provided to be used within the 'crit' header MUST be unique");
+            
+            if(_jsonPropertyNamesDefinedByJws.Intersect(criticalHeaders).Any())
+                throw new ArgumentException("Values provided to be used within the 'crit' header MUST not overlap with any header name defined in RFC7515");
+
+            if (additionalHeaders is null || !criticalHeaders.All(additionalHeaders.ContainsKey))
+                throw new ArgumentException("Values provided to be used within the 'crit' header MUST also be listed as additional headers");
         }
 
         /*
